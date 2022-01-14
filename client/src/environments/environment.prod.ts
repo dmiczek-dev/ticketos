83eaee1b1a203ecd1a56447b21b1093f0f0903d3
Dawid Miczek <dmiczek52@gmail.com>
2022-01-14 11:29:23 +0100

    safty update

--- client/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  socket_address: 'http://192.168.3.243:3000',
-  api_address: 'http://192.168.3.243:3000/api',
-  googleApiKey: 'AIzaSyBvafpEHqBfqXekyhvYKQTA3bIHr2Q24gI',
-};
